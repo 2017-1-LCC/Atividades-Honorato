8fe089291c113d1257d5460e56889b429d3f8df0
Fabianne Costa <fabiannecs@hotmail.com>
2017-09-15 19:04:17 -0300

    Create 12 - Fernando.c

---
+++ 12 - Fernando.c
@@ -0,0 +1,67 @@
+// Atividade Honorato - Questão 12
+
+#include <stdio.h>
+#include <stdlib.h>
+#define tam 3
+
+int L,C;
+
+void menor (int mat[tam][tam]){
+
+    int mn=mat[0][0];
+
+    for (L=0;L<tam;L++){
+		for (C=0;C<tam;C++){
+
+    if (mat[L][C]<mn){
+        mn=mat[L][C];
+            }
+        }
+
+    }
+    printf ("\nMenor: %d",mn);
+
+}
+void maior (int mat[tam][tam]){
+    int ma=mat[0][0];
+
+    for (L=0;L<tam;L++){
+		for (C=0;C<tam;C++){
+
+    if (mat[L][C]>ma){
+        ma=mat[L][C];
+            }
+        }
+
+    }
+    printf ("\nMaior: %d",ma);
+}
+
+
+main (){
+	int MAT[tam][tam];
+
+
+	printf("Preenchendo a matriz MAT\n");
+	for (L=0;L<tam;L++){
+		printf("\nPreenchendo a linha %d\n",L);
+		for (C=0;C<tam;C++){
+			printf("Informe o valor da coluna |%d| : ",C);
+			scanf("%d",&MAT[L][C]);
+		}
+	}
+
+
+	for (L=0;L<tam;L++){
+		for (C=0;C<tam;C++){
+			printf("\nlinha |%d| coluna |%d|: [%d]",L,C,MAT[L][C]);
+		}
+	}
+
+	menor(MAT);
+	maior (MAT);
+
+
+	 system("cls"); //usado para limpar a tela
+
+}
