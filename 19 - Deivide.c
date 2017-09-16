a368a4627e9de8a584dca44c37e0314f6ca31851
Fabianne Costa <fabiannecs@hotmail.com>
2017-09-16 17:51:10 -0300

    Update 19 - Deivide.c

--- 19 - Deivide.c
+++ 19 - Deivide.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 #define t 3
-int menu(){                                                      //menu
+int menu(){            //menu
 	int r;
 	printf("\n\t\tEscolha a funcao a ser executada :\n");
 	printf("1-SOMA\n");
@@ -25,20 +25,20 @@ int soma(int mat[][t], int m[][t], int n[][t]){   //soma
 int todos(int mat[t][t], int m[t][t], int n[t][t]){  //todos os elementos
 	int l,c;
 	printf("'Matriz MAT'\n");
-	for (l=0;l<t;l++){                                        //exibindo os valores da matriz MAT
+	for (l=0;l<t;l++){               //exibindo os valores da matriz MAT
 		for(c=0;c<t;c++){
 			printf("    |%d| ",mat[l][c]);
 		}
 		printf("\n");
  	}
-	printf("'Matriz M'\n");                                      //exibindo os valores da matriz M
+	printf("'Matriz M'\n");       //exibindo os valores da matriz M
 	for (l=0;l<t;l++){
 		for(c=0;c<t;c++){
 			printf("    |%d| ",mat[l][c]);
 		}
 		printf("\n");
 	 }
-	printf("'Matriz N'\n");                                      //exibindo os valores da matriz N
+	printf("'Matriz N'\n");       //exibindo os valores da matriz N
 	for (l=0;l<t;l++){
 		for(c=0;c<t;c++){
 			printf("    |%d| ",mat[l][c]);
@@ -47,9 +47,9 @@ int todos(int mat[t][t], int m[t][t], int n[t][t]){  //todos os elementos
 	}
 }
 
-main(){                                                      //função principal
+main(){              //função principal
 	int l,r,c,mat[t][t],n[t][t],m[t][t];
-	printf("'Matriz MAT'");                                      //preenchendo os valores da matriz MAT
+	printf("'Matriz MAT'");     //preenchendo os valores da matriz MAT
 	for (l=0;l<t;l++){
 		printf("\t\tLinha %d\n",l+1);
 		for(c=0;c<t;c++){
@@ -57,7 +57,7 @@ main(){                                                      //função principa
 			scanf("%d",&mat[l][c]);	
 		}
 	}
-	printf("'Matriz M'");                                        //preenchendo os valores da matriz M
+	printf("'Matriz M'");      //preenchendo os valores da matriz M
 	for (l=0;l<t;l++){
 		printf("\t\tLinha %d\n",l+1);
 		for(c=0;c<t;c++){
@@ -65,7 +65,7 @@ main(){                                                      //função principa
 			scanf("%d",&m[l][c]);	
 		}
 	}   
-	printf("'Matriz N'");                                        //preenchendo os valores da matriz N
+	printf("'Matriz N'");      //preenchendo os valores da matriz N
 	for (l=0;l<t;l++){
 		printf("\t\tLinha %d\n",l+1);
 		for(c=0;c<t;c++){
@@ -73,7 +73,7 @@ main(){                                                      //função principa
 			scanf("%d",&n[l][c]);	
 		}
 	}
-	r=menu();                                                    //chamando a função menu
+	r=menu();                //chamando a função menu
 	while(r!=3) {
 		if(r==1) soma(mat,m,n);
 		if(r==2) todos(mat,m,n);
