23675e9540e35ede74d691c65b2de4bd252970d6
Fabianne Costa <fabiannecs@hotmail.com>
2017-09-16 17:42:40 -0300

    Add files via upload

---
+++ Matriz e a matriz transposta.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int Transposta (int matriz[3][3]){
+//Variaveis
+ 	int MT[3][3];
+ 	int l,c;
+ 	
+ 	for(l=0; l<3; l++)
+	{
+		for(c=0; c<3; c++)
+		{
+			MT[l][c]= matriz[c][l];		
+		}	
+	} 	
+ 	printf("\n\n\tMatriz\n\n");
+//Procedimento	
+	for(l=0; l<3; l++)
+	{
+		for(c=0; c<3; c++)
+		{		
+			printf("%d\t",matriz[l][c]);	
+		}
+		printf("\n");
+	}
+	printf("\n\n Matriz Transposta\n\n");
+	for(l=0; l<3; l++)
+	{
+		for(c=0; c<3; c++)
+		{
+			printf("%d\t",MT[l][c]);
+		}
+		printf("\n");
+	}
+	
+	 printf("\n\n");	 
+		  
+ 	
+ }
+ main (){
+ // Variaveis	
+	int matriz[3][3];		
+	int l, c;
+	int transposta[3][3];
+	printf("\t\t\t***Preenchendo a Matriz***\n\n");
+ //Procedimentos   					
+   for(l=0; l<3; l++)
+   {    		  
+   	  	for(c=0; c<3; c++)
+		{
+	   	 	printf("Digite o valor: ");				  	
+	   		scanf("%d", &matriz[l][c]);	 
+		}
+		
+    }
+    Transposta(matriz);
+	
+
+	system("pause");
+  	             
+}
+
+
+	
